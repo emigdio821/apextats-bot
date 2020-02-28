3935cb3a81200be7703883247b174dd864c7492a
Emigdio Torres Ruiz <emigdio821@gmail.com>
2020-02-27 19:25:03 -0600

    finished most of the code using an API

--- commands/statsCommand.js
+++ commands/statsCommand.js
@@ -1,4 +1,6 @@
+const fetch = require("node-fetch");
 const Discord = require("discord.js");
+const { apexToken } = require("../config-files/config.json");
 
 module.exports = {
   onShowStats: (message, options) => {
@@ -7,7 +9,8 @@ module.exports = {
         embed: {
           author: {
             name: "OOPS!",
-            icon_url: "https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/caution-512.png"
+            icon_url:
+              "https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/caution-512.png"
           },
           description:
             "Seems like you are missing an option, please review the available" +
@@ -20,31 +23,86 @@ module.exports = {
       return;
     }
 
-    const embed = new Discord.RichEmbed()
-      .setAuthor(
-        "Xtats Bot",
-        "https://secure.download.dm.origin.com/production/avatar/prod/userAvatar/27625359/208x208.JPEG"
-      )
-      .setTitle("LilToadd @PC/Origin")
-      .setURL("https://github.com/emigdio821")
-      .setThumbnail(
-        "https://trackercdn.com/cdn/apex.tracker.gg/legends/lifeline-tile.png"
-      )
-      // .setColor("#FC3903")
-      .setDescription(
-        "Seems like you are doing a great job playing **Lifeline**, keep carrying your teammates (jk) :wink:. \n\n" +
-          "Here you have some of your Apex Legends stats:"
-      )
-      .setImage(
-        "https://trackercdn.com/cdn/apex.tracker.gg/legends/lifeline-concept-bg-small.jpg"
-      )
-      .addField("**Kills**", "3492", true)
-      // .addBlankField(true)
-      .addField("**Deaths**", "234", true)
-      .addField("**Assists**", "3452", true)
-      .setFooter("Powered by apex.tracker.gg")
-      .setTimestamp();
+    const platform = getPlatform(options[1].toLowerCase());
+    const url = `https://public-api.tracker.gg/apex/v1/standard/profile/${platform}/${options[0]}`;
 
-    message.channel.send({ embed });
+    fetch(url, {
+      method: "get",
+      headers: { "TRN-Api-Key": apexToken }
+    })
+      .then(res => res.json())
+      .then(json => {
+        if (json.errors) {
+          message.channel.send({
+            embed: {
+              author: {
+                name: "OOPS!",
+                icon_url:
+                  "https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/caution-512.png"
+              },
+              description: json.errors[0].message
+            }
+          });
+
+          return;
+        }
+        const data = json.data;
+        const stats = data.stats;
+        const level = getObjByKey(stats, "Level");
+        const kills = getObjByKey(stats, "Kills");
+        const rankScore = getObjByKey(stats, "RankScore");
+        const embed = new Discord.RichEmbed()
+          .setAuthor("Xtats Bot", data.metadata.avatarUrl)
+          .setTitle(
+            `${
+              data.metadata.platformUserHandle
+            } | ${options[1].toUpperCase()} | ${data.metadata.rankName}`
+          )
+          .setURL(
+            `https://apex.tracker.gg/apex/search?name=${options[0]}&platform=${platform}`
+          )
+          .setThumbnail(data.metadata.rankImage)
+          // .setColor("#FC3903")
+          .setDescription(
+            `Seems like you are doing a great job playing **${data.children[0].metadata.legend_name}**, keep carrying your teammates (or not) :wink:. \n\n` +
+              "Here you have some of your Apex Legends stats:"
+          )
+          .setImage(data.children[0].metadata.bgimage)
+          .addField(`**${level.metadata.name}**`, level.displayValue, true)
+          .addField(`**${kills.metadata.name}**`, kills.displayValue, true)
+          // .addBlankField(true)
+          .addField(
+            `**${rankScore.metadata.name}**`,
+            rankScore.displayValue,
+            true
+          )
+          .setFooter(
+            "Powered by apex.tracker.gg",
+            data.children[0].metadata.icon
+          )
+          .setTimestamp();
+
+        message.channel.send({ embed });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
+};
+
+var getPlatform = platform => {
+  let platformCode;
+  if (platform === "xbox") {
+    platformCode = 1;
+  } else if (platform === "ps") {
+    platformCode = 2;
+  } else if (platform === "origin" || platform === "pc") {
+    platformCode = 5;
+  }
+
+  return platformCode;
+};
+
+var getObjByKey = (array, key) => {
+  return array.find(element => element.metadata.key === key);
 };
